dd21fe77ef386ea7c235d2f65868eb984e0ff375
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-03-25 14:16:00 +0100

    Initial Commit

---
+++ TouchPad/TouchPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchPadApp.swift
+//  TouchPad
+//
+//  Created by Philipp Hemkemeyer on 25.03.22.
+//
+
+import SwiftUI
+
+@main
+struct TouchPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
