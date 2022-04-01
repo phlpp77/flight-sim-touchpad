e407c1c8fc767aa84a23ac648d133ad6203f4e39
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-04-01 12:23:55 +0200

    Add initial settings view

---
+++ TouchPad/SettingsView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsView.swift
+//  TouchPad
+//
+//  Created by Philipp Hemkemeyer on 01.04.22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .previewDevice("iPad Pro (11-inch) (3rd generation)")
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
