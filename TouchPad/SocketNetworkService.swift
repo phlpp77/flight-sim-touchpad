f01add8d9740d84b73863d79359525ea77b70675
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-04-28 14:19:51 +0200

    Cleanup of SocketNetworkService

--- TouchPad/SocketNetworkService.swift
+++ TouchPad/SocketNetworkService.swift
@@ -12,39 +12,47 @@ class SocketNetworkService: NSObject, URLSessionWebSocketDelegate {
     var webSocket: URLSessionWebSocketTask?
     @Published var isConnectionOpen = false
     
-    override init() {
-        super.init()
-        
-        let urlString = "ws://192.168.103.103:2048/fsuipc/"
-        //        let urlString = "ws://localhost:2048"
-        
-        if let url = URL(string: urlString) {
+    // MARK: - Open and close connection
+    
+    // MARK: Open WebSocket connection
+    func openWebSocket() {
+        if let url = URL(string: Constants.URL_STRING) {
             let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
             webSocket = session.webSocketTask(with: url, protocols: ["fsuipc"])
-            print("[SocketNetworkService] WebSocket session started")
+            print("[openWebSocket] WebSocket session started on URL: \(url)")
         }
-    }
-    
-    func openWebSocket() {
         webSocket?.resume()
-        print("[SocketNetworkService] Try to resume/open webSocket")
+        print("[openWebSocket] Resume WebSocket session")
     }
     
+    // MARK: Close WebSocket connection
     func closeWebSocket() {
         //        webSocket?.cancel(with: <#T##URLSessionWebSocketTask.CloseCode#>, reason: <#T##Data?#>)
     }
     
     // MARK: - Send and Receive
     
+    // MARK: Send a message as String
     func sendString(_ message: String) {
-
+        
         webSocket?.send(URLSessionWebSocketTask.Message.string(message)) { error in
             if let error = error {
-                print("[SendString] Failed to send with error \(error.localizedDescription)")
+                print("[sendString] Failed to send with error \(error.localizedDescription)")
             }
         }
     }
     
+    // MARK: Send data as Data
+    func sendData(_ data: Data) {
+        
+        webSocket?.send(URLSessionWebSocketTask.Message.data(data)) { error in
+            if let error = error {
+                print("[sendData] Failed to send with error \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    // MARK: Send offset as OffsetType of Flight Simulator
     func sendOffset<OffsetType>(_ offsetPackage: Offsets<OffsetType>) {
         
         let jsonEncoder = JSONEncoder()
@@ -70,19 +78,9 @@ class SocketNetworkService: NSObject, URLSessionWebSocketDelegate {
             print("[SendOffset] \(error.localizedDescription)")
         }
         
-        
     }
     
-    func sendData(_ data: Data) {
-        
-        print("[SendDate] Connection open: \(isConnectionOpen)")
-        webSocket?.send(URLSessionWebSocketTask.Message.data(data)) { error in
-            if let error = error {
-                print("[SocketNetworkService] SendData failed with error \(error.localizedDescription)")
-            }
-        }
-    }
-    
+    // MARK: Receive message as String
     func receiveMessage() -> String {
         
         var outputMessage = "Empty"
@@ -114,19 +112,20 @@ class SocketNetworkService: NSObject, URLSessionWebSocketDelegate {
         return outputMessage
     }
     
-    // MARK: - Flight simulator functions
+    // MARK: - Flight simulator
     
     // MARK: Declare offsets
     func declareOffsets() {
         print("[declareOffsets] start")
-        let declareOffsets = Offsets(command: "offsets.declare",
-                                     name: "OffsetsWrite",
-                                     offsets: [
-                                        DeclareOffset(name: "speed", address: 16906, type: "int", size: 2),
-                                        DeclareOffset(name: "altitude", address: 16908, type: "int", size: 2),
-                                        DeclareOffset(name: "heading", address: 16910, type: "int", size: 2),
-                                        DeclareOffset(name: "TurnFactor", address: 16912, type: "int", size: 1)
-                                     ]
+        let declareOffsets = Offsets(
+            command: "offsets.declare",
+            name: "OffsetsWrite",
+            offsets: [
+                DeclareOffset(name: "speed", address: 16906, type: "int", size: 2),
+                DeclareOffset(name: "altitude", address: 16908, type: "int", size: 2),
+                DeclareOffset(name: "heading", address: 16910, type: "int", size: 2),
+                DeclareOffset(name: "TurnFactor", address: 16912, type: "int", size: 1)
+            ]
         )
         
         sendOffset(declareOffsets)
@@ -141,9 +140,16 @@ class SocketNetworkService: NSObject, URLSessionWebSocketDelegate {
         print("[changeSpeed] answer: \(receiveMessage())")
     }
     
+    // MARK: - Constants
+    
+    private struct Constants {
+        static let URL_STRING = "ws://192.168.103.103:2048/fsuipc/"
+        //        let urlString = "ws://localhost:2048"
+    }
+    
 }
 
-// Extension for callback functions
+// MARK: - Extension for callback functions
 extension SocketNetworkService {
     func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
         print("[SocketNetworkService] WebSocket connection opened")
