bb062d954ed8a4df037298d63e15778e6777ce87
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-05-02 13:02:15 +0200

    Add local dataformatter

--- TouchPad/DateExtension.swift
+++ TouchPad/DateExtension.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+extension Date {
+    func localFlightSim() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.init(abbreviation: "CEST")
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss.SSS ZZZZ"
+        return dateFormatter.string(from: self)
+    }
+}
