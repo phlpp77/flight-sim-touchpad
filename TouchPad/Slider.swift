e5cec2ebedb736b7351e813ec6aed36912810caa
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-05-02 15:55:49 +0200

    Update slider

--- TouchPad/Slider.swift
+++ TouchPad/Slider.swift
@@ -23,19 +23,25 @@ struct SliderView: View {
             ValueSlider(value: $speed, in: 100...399, step: 1, onEditingChanged: {editing in
                 print("end editing: \(editing)")
                 if !editing {
-                    socketNetworkVM.setSpeed(to: speed)
+//                    socketNetworkVM.setSpeed(to: speed)
                 }
             })
+            
+            
             .valueSliderStyle(
 
                 VerticalValueSliderStyle(
-                    track: VerticalValueTrack(view: Rectangle(), mask: RoundedRectangle(cornerRadius: 12)),
+                    track: VerticalValueTrack(view: RoundedRectangle(cornerRadius: 40), mask: RoundedRectangle(cornerRadius: 12)),
 //                    track: Rectangle().foregroundColor(.gray),
                     thumb: Rectangle()
+                        .onTapWithLocation { location in
+                            print("Button pressed at location: \(location) at \(Date().localFlightSim())")
+                        }
                         .frame(width: 200, height: 80, alignment: .leading)
                         .foregroundColor(.gray)
                         .mask(RoundedRectangle(cornerRadius: 12))
-                ))
+                )
+            )
         }
         .frame(width: 200)
     }
@@ -45,6 +51,7 @@ struct Slider_Previews: PreviewProvider {
     static var previews: some View {
         let socketNetworkVM = SocketNetworkViewModel()
         SliderView(socketNetworkVM: socketNetworkVM)
+            .previewDevice("iPad Pro (11-inch) (3rd generation)")
             .previewInterfaceOrientation(.landscapeLeft)
     }
 }
