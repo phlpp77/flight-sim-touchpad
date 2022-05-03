b87ab073ede1774bf0e5774ebcb07096706c5861
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-05-03 09:26:54 +0200

    Change Slider to get pos of thumb 

--- TouchPad/Slider.swift
+++ TouchPad/Slider.swift
@@ -14,36 +14,45 @@ struct SliderView: View {
     
     @State private var speed = 300
     @State private var isEditing = false
+    @State private var pos = CGPoint(x: 0, y: 0)
+    @State private var thumbPos = CGPoint(x: 0, y: 0)
     
     var body: some View {
-        VStack {
-            Text("Speed \(speed)")
-                .font(.largeTitle)
-            
-            ValueSlider(value: $speed, in: 100...399, step: 1, onEditingChanged: {editing in
-                print("end editing: \(editing)")
-                if !editing {
-//                    socketNetworkVM.setSpeed(to: speed)
-                }
-            })
-            
-            
-            .valueSliderStyle(
+        ZStack {
+            VStack {
+//                Text("Speed \(speed)")
+//                    .font(.largeTitle)
+                
+                ValueSlider(value: $speed, in: 100...399, step: 1, onEditingChanged: {editing, values in
+                    print("end editing: \(editing) start loc: \(values)")
+                    pos = values.location
+                    pos.x += 200
+                    print("thumb pos: \(thumbPos)")
+                    if !editing {
+    //                    socketNetworkVM.setSpeed(to: speed)
+                    }
+                })
+                .valueSliderStyle(
 
-                VerticalValueSliderStyle(
-                    track: VerticalValueTrack(view: RoundedRectangle(cornerRadius: 40), mask: RoundedRectangle(cornerRadius: 12)),
-//                    track: Rectangle().foregroundColor(.gray),
-                    thumb: Rectangle()
-                        .onTapWithLocation { location in
-                            print("Button pressed at location: \(location) at \(Date().localFlightSim())")
-                        }
-                        .frame(width: 200, height: 80, alignment: .leading)
-                        .foregroundColor(.gray)
-                        .mask(RoundedRectangle(cornerRadius: 12))
+                    VerticalValueSliderStyle(
+                        track: VerticalValueTrack(view: RoundedRectangle(cornerRadius: 40), mask: RoundedRectangle(cornerRadius: 12)),
+                        thumb:
+                            GeometryReader { geo in
+                                Rectangle()
+                                .frame(width: 150, height: 80, alignment: .leading)
+                                .foregroundColor(.gray)
+                                .mask(RoundedRectangle(cornerRadius: 12))
+                            }
+                            
+                    )
                 )
-            )
+            }
+            .frame(width: 200)
         }
-        .frame(width: 200)
+        Rectangle()
+            .foregroundColor(.red)
+            .frame(width: 20, height: 20)
+            .position(pos)
     }
 }
 
