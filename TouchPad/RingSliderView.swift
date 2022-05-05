b2d14138f5a02b749ca37d20301aabf3d34b2e54
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-05-05 17:01:12 +0200

    Cleanup of RingSlider

--- TouchPad/RingSliderView.swift
+++ TouchPad/RingSliderView.swift
@@ -22,6 +22,10 @@ struct RingSliderView: View {
         GeometryReader { geo in
             ZStack {
                 Circle()
+                    .stroke(lineWidth: 12)
+                    .opacity(0.3)
+                
+                Circle()
                     .trim(from: 0, to: progress)
                     .stroke(style: StrokeStyle(lineWidth: 12, lineCap: .round))
                     .rotationEffect(.degrees(-90))
@@ -32,15 +36,11 @@ struct RingSliderView: View {
                         DragGesture(coordinateSpace: .named("RingSlider"))
                             .onChanged { actions in
                                 
-//                                var center: CGPoint = .zero
-//                                center.y -= geo.size.width / 2
-//                                center.x -= geo.size.height / 2
-                                
+                                // create vectors
                                 let center = SIMD2<Double>(x: geo.size.width / 2, y: geo.size.height / 2)
                                 let intersection = SIMD2<Double>(x: actions.location.x, y: actions.location.y)
                                 
-                                print("center and intersec: \(center) \(intersection)")
-                                
+                                // every time a new drag starts the start-point is resetted
                                 if !isDragging {
                                     startInteraction(intersection: intersection, center: center)
                                 } else {
@@ -49,38 +49,16 @@ struct RingSliderView: View {
                                 
                                 isDragging = true
                                 
-                                
+                                // position of marker
                                 markerPos = actions.location
                                 markerPos.y -= geo.size.width / 2
                                 markerPos.x -= geo.size.height / 2
-                                
-                                
-                                
-                                
-//                                print("marker pos x: \(markerPos.x / geo.size.width * 2)")
-//                                print("marker pos y: \(markerPos.y / geo.size.width * 2)")
-                                
-                                
-//                                var localCoords = actions.location
-//                                localCoords.y -= geo.size.width / 2
-//                                localCoords.x -= geo.size.height / 2
-//                                localCoords.x = localCoords.x / geo.size.width * 2
-//                                localCoords.y = localCoords.y / geo.size.height * 2
-//
-//                                // change coordinate system from standard unit circle to swiftUI
-//                                localCoords.y *= -1
-//
-//                                print("local cords: \(localCoords)")
-//                                let degree = coord2degree(localCoords)
-//                                print("degree: \(degree / 360)")
-//                                progress = degree / 360 <= 0.99 ? CGFloat(degree / 360) : 1
-                                
                                 
                             }
                             .onEnded { _ in
                                 isDragging = false
                             }
-                            
+                        
                     )
                     .simultaneousGesture(
                         DragGesture(coordinateSpace: .named("Circle"))
@@ -89,16 +67,16 @@ struct RingSliderView: View {
                                 relativeDeviation.x -= circleDiameter / 2
                                 relativeDeviation.y -= circleDiameter / 2
                                 
-                                print("relative deviation: \(relativeDeviation)")
+                                print("relative deviation: \(relativeDeviation)") // FIXME: log print here
                             }
                     )
                     .coordinateSpace(name: "Circle")
                     .frame(width: circleDiameter, height: circleDiameter)
                     .offset(y: -geo.size.width / 2)
                     .rotationEffect(Angle.degrees(360 * Double(progress)))
-                    
                 
                 
+                // MARK: Marker
                 Rectangle()
                     .fill(.green)
                     .position(markerPos)
@@ -107,43 +85,30 @@ struct RingSliderView: View {
                         DragGesture()
                             .onChanged { actions in
                                 markerPos = actions.location
-                                print("marker pos x: \(markerPos.x / geo.size.width * 2)")
                             }
                     )
             }
         }
-        .background(Color.red)
         .coordinateSpace(name: "RingSlider")
-        
         .aspectRatio(contentMode: .fit)
-        
-        
     }
     
     
-    /// start Interaction
+    /// Initialize the the angle calculation
     func startInteraction(intersection: SIMD2<Double>, center: SIMD2<Double>) {
-        // `intersection`: momentane touch-position
         vStart = normalize(intersection - center)
     }
-
+    
+    /// Fulfill and update the angle calculation
     func changeInteraction(intersection: SIMD2<Double>, center: SIMD2<Double>) {
         let vEnd = normalize(intersection - center)
         let d = simd_clamp(dot(vStart, vEnd), -1.0, 1.0)
         var angle = cross(vStart, vEnd).z > 0 ? acos(d) : -acos(d)
         angle /= 2 * Double.pi
         
+        // only take angles between 0 and 1
         progress = simd_clamp(progress + angle, 0, 1)
-//        progress += angle
         vStart = vEnd
-        
-//        let difference = angle - lastAngle
-//        print(cross(vStart, vEnd))
-//        progress += difference / (2 * Double.pi)
-//        print("progress: \(progress) \(difference)")
-//        lastAngle = angle
-
-        // zielvariable um `difference` erhöhen
     }
     
 }
