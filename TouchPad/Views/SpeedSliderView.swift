7b42968dc0384afeb027604836cbd9ea59ae0ac2
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-05-09 16:07:50 +0200

    Start of speed slider

---
+++ TouchPad/Views/SpeedSliderView.swift
@@ -0,0 +1,47 @@
+//
+//  SpeedSliderView.swift
+//  TouchPad
+//
+//  Created by Philipp Hemkemeyer on 09.05.22.
+//
+
+import SwiftUI
+
+struct SpeedSliderView: View {
+    
+    var range = 1...4
+    
+    var body: some View {
+        VStack(spacing: 5.0) {
+            ForEach(range.reversed(), id: \.self) { item in
+                VStack(alignment: .leading, spacing: 5.0) {
+                    HStack {
+                        Rectangle()
+                            .frame(width: 18, height: 1)
+                        Text("\(item)00")
+                            .frame(height: 1)
+                    }
+                    VStack(alignment: .leading, spacing: 2.0) {
+                    ForEach(1...4, id: \.self) { _ in
+                    
+                        Rectangle()
+                            .frame(width: 18, height: 1)
+                        VStack(spacing: 2.0) {
+                            ForEach(1...9, id: \.self) { _ in
+                                Rectangle()
+                                    .frame(width: 8, height: 1)
+                            }
+                        }
+                    }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct SpeedSliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeedSliderView()
+    }
+}
