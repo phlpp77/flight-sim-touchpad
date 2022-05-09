792f8e8aeb2f985b9e89a4e0515669a726e057ef
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-05-09 15:39:02 +0200

    Fix turn factor picker

--- TouchPad/Views/HeadingView.swift
+++ TouchPad/Views/HeadingView.swift
@@ -19,45 +19,51 @@ struct HeadingView: View {
             
             VStack {
                 Text("Direction of turn")
-                HStack {
-                    Spacer()
-                    Button(action:  {
-                        turnFactor = 0
-                    }) {
-                        Image(systemName: "arrow.turn.up.left")
-                    }
-                    Spacer()
-                    ZStack {
+                ZStack {
+                    
+                    // MARK: Indication Overlay
+                    Capsule()
+                        .frame(width: 100, height: 50)
+                        .foregroundStyle(
+                            LinearGradient(gradient: Gradient(colors: [Color(hexCode: "828282")!, Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .overlay(
+                            Capsule()
+                                .stroke(
+                                    LinearGradient(colors: [Color.white, Color(hexCode: "B9B9B9")!], startPoint: .topLeading, endPoint: .bottomTrailing),
+                                    lineWidth: 4
+                                ))
+                        .overlay(
+                            Capsule()
+                                .stroke(
+                                    LinearGradient(colors: [Color(hexCode: "B9B9B9")!, Color.white], startPoint: .topLeading, endPoint: .bottomTrailing),
+                                    lineWidth: 2
+                                ))
+                        .offset(x: turnFactor != -1 ? (turnFactor == 0 ? -95 : 95) : 0)
+                        .animation(.interactiveSpring(), value: turnFactor)
+                    
+                    // MARK: Buttons
+                    LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                        
+                        Button(action:  {
+                            turnFactor = 0
+                        }) {
+                            Image(systemName: "arrow.turn.up.left")
+                                .foregroundColor(turnFactor == 0 ? .black : .white)
+                        }
+                        
                         Image(systemName: "diamond.fill")
                             .font(.title3)
-                        Capsule()
-                            .frame(width: 100, height: 50)
-                            .foregroundStyle(
-                                LinearGradient(gradient: Gradient(colors: [Color("828282"), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
-                            )
-                            .overlay(
-                                Capsule()
-                                    .stroke(
-                                        LinearGradient(colors: [Color.white, Color("B9B9B9")], startPoint: .topLeading, endPoint: .bottomTrailing),
-                                        lineWidth: 4
-                                    ))
-                            .overlay(
-                                Capsule()
-                                    .stroke(
-                                        LinearGradient(colors: [Color("B9B9B9"), Color.white], startPoint: .topLeading, endPoint: .bottomTrailing),
-                                        lineWidth: 2
-                                    ))
-                            .offset(x: turnFactor != -1 ? (turnFactor == 0 ? -95 : 95) : 0)
-                            .animation(.interactiveSpring(), value: turnFactor)
+                            .foregroundColor(turnFactor == -1 ? .black : .white)
                         
+                        Button(action:  {
+                            turnFactor = 1
+                        }) {
+                            Image(systemName: "arrow.turn.up.right")
+                                .foregroundColor(turnFactor == 1 ? .black : .white)
+                        }
                     }
-                    Spacer()
-                    Button(action:  {
-                        turnFactor = 1
-                    }) {
-                        Image(systemName: "arrow.turn.up.right")
-                    }
-                    Spacer()
+                    
                 }
                 .padding(6)
                 .font(.largeTitle)
