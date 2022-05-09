4faf3d58d9a031d9144a0f6e00c060b586202dc5
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-05-09 10:54:23 +0200

    Add HeadingView and Color extension

---
+++ TouchPad/Views/HeadingView.swift
@@ -0,0 +1,83 @@
+//
+//  HeadingView.swift
+//  TouchPad
+//
+//  Created by Philipp Hemkemeyer on 09.05.22.
+//
+
+import SwiftUI
+
+struct HeadingView: View {
+    
+    @State var turnFactor = 0
+    
+    var body: some View {
+        VStack {
+            
+            
+            VStack {
+                Text("Direction of turn")
+                HStack {
+                    Spacer()
+                    Button(action:  {
+                        turnFactor = -1
+                    }) {
+                        Image(systemName: "arrow.turn.up.left")
+                    }
+                    Spacer()
+                    ZStack {
+                        Image(systemName: "diamond.fill")
+                            .font(.title3)
+                        Capsule()
+                            .frame(width: 100, height: 50)
+                            .foregroundStyle(
+                                LinearGradient(gradient: Gradient(colors: [Color("828282"), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            )
+                            .overlay(
+                                Capsule()
+                                    .stroke(
+                                        LinearGradient(colors: [Color.white, Color("B9B9B9")], startPoint: .topLeading, endPoint: .bottomTrailing),
+                                        lineWidth: 4
+                                    ))
+                            .overlay(
+                                Capsule()
+                                    .stroke(
+                                        LinearGradient(colors: [Color("B9B9B9"), Color.white], startPoint: .topLeading, endPoint: .bottomTrailing),
+                                        lineWidth: 2
+                                    ))
+                            .offset(x: turnFactor != 0 ? (turnFactor == -1 ? -95 : 95) : 0)
+                            .animation(.interactiveSpring(), value: turnFactor)
+                        
+                    }
+                    Spacer()
+                    Button(action:  {
+                        turnFactor = 1
+                    }) {
+                        Image(systemName: "arrow.turn.up.right")
+                    }
+                    Spacer()
+                }
+                .padding(6)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .frame(width: 300)
+                .background(Color.black)
+                .mask(Capsule())
+            }
+            
+            RingSliderView()
+                .padding(.top, 30)
+                .padding()
+                .background(Color.black.opacity(0.8))
+                .mask(RoundedRectangle(cornerRadius: 22))
+        }
+        .frame(width: 350)
+    }
+}
+
+struct HeadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeadingView()
+            .previewInterfaceOrientation(.landscapeLeft)
+    }
+}
