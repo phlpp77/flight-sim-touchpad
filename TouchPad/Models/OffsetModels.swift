297e8c8251da5131469acc868cbeadd0ad6e360a
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-06-16 12:50:18 +0200

    Delete unused code

--- TouchPad/Models/OffsetModels.swift
+++ TouchPad/Models/OffsetModels.swift
@@ -27,35 +27,3 @@ struct WriteOffset: Encodable {
     var name: String
     var value: Int
 }
-
-
-//struct OffsetsDeclare: Encodable {
-//    var command = "offsets.declare"
-//    var name = "OffsetsWrite"
-//    var offsets = [
-//        Offset(name: "speed", address: 16906, type: "int", size: 2),
-//        Offset(name: "altitude", address: 16908, type: "int", size: 2),
-//        Offset(name: "heading", address: 16910, type: "int", size: 2),
-//        Offset(name: "TurnFactor", address: 16912, type: "int", size: 1)
-//    ]
-//    
-//    struct Offset: Encodable {
-//        var name: String
-//        var address: Int
-//        var type: String
-//        var size: Int
-//    }
-//}
-
-//struct OffsetsWrite: Encodable {
-//    var command = "offsets.write"
-//    var name = "OffsetsWrite"
-//    var offsets = [
-//        Offset(name: "speed", value: 222)
-//    ]
-//    
-//    struct Offset: Encodable {
-//        var name: String
-//        var value: Int
-//    }
-//}
