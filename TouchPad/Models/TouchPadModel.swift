77d1b93f879057ba1ad37b22e9c49f2e1fb4909a
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-06-22 10:17:47 +0200

    Create model for setting new values

--- TouchPad/Models/TouchPadModel.swift
+++ TouchPad/Models/TouchPadModel.swift
@@ -10,6 +10,7 @@ import Foundation
 struct TouchPadModel {
     
     private(set) var settings = TouchPadSettings()
+    private(set) var startValues = AircraftStartValues()
     
     struct TouchPadSettings {
         var showTapIndicator: Bool = false
@@ -17,6 +18,15 @@ struct TouchPadModel {
         var headingStepsInFive: Bool = true
         var screen: Screen = .essential
         var webSocketConnectionIsOpen: Bool = false
+    }
+    
+    struct AircraftStartValues {
+        var speed: Int = 250
+        var heading: Double = 0
+        var altitude: Int = 10000
+        var flaps: Int = 0
+        var gear: Int = 0
+        var spoiler: Int = 0
     }
     
     mutating func changeTapIndicator(_ newState: Bool) {
@@ -35,4 +45,20 @@ struct TouchPadModel {
         settings.screen = newState
     }
     
+    mutating func changeStartValues(of valueType: AircraftDataType, to value: Int) {
+        switch valueType {
+        case .speed:
+            startValues.speed = value
+        case .altitude:
+            startValues.altitude = value
+        case .heading:
+            startValues.heading = Double(value)
+        case .flaps:
+            startValues.flaps = value
+        case .gear:
+            startValues.gear = value
+        case .spoiler:
+            startValues.spoiler = value
+        }
+    }
 }
