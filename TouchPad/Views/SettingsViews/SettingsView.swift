b7b5840e5ef75ab14ae843730a7ef1907669e626
phlpp77 <PhilippHemkemeyer@iCloud.com>
2022-07-01 18:10:02 +0200

    Change version to v2.0.1-beta

--- TouchPad/Views/SettingsViews/SettingsView.swift
+++ TouchPad/Views/SettingsViews/SettingsView.swift
@@ -138,7 +138,7 @@ struct SettingsView: View {
                     HStack {
                         Text("Version")
                         Spacer()
-                        Text("v2.0.0-beta")
+                        Text("v2.0.1-beta")
                             .foregroundColor(.gray)
                     }
                     HStack {
